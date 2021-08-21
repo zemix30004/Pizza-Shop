b6625f25be7b1271d0df092f5471f07e72ec678b
zemix30004 <eugenezm@gmail.com>
2021-08-21 16:14:07 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
 
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/categories', function () {
+
+    return view('categories');
 });

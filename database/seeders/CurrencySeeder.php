b5a433dcfcac7e8188be6527513a4c321a68f87a
zemix30004 <eugenezm@gmail.com>
2021-09-01 11:08:51 +0300

    initial commit

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('currencies')->truncate();
+
+        DB::table('currencies')->insert([
+            [
+                'code' => 'GRN',
+                'symbol' => '₴',
+                'is_main' => 1,
+                'rate' => 1,
+
+            ],
+            [
+                'code' => 'USD',
+                'symbol' => '$',
+                'is_main' => 0,
+                'rate' => 28,
+
+            ],
+            [
+                'code' => 'EUR',
+                'symbol' => '€',
+                'is_main' => 0,
+                'rate' => 28,
+
+            ],
+        ]);
+    }
+}
